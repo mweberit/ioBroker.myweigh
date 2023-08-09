81807470926646f37786faddd96734bc66ebe7de
mweberit <141611245+mweberit@users.noreply.github.com>
2023-08-09 22:07:11 +0200

    Update main.js

--- main.js
+++ main.js
@@ -237,6 +237,7 @@ class Myweigh extends utils.Adapter {
 						port.write(buffer);
 
 						if (!id.endsWith(".getData")) {
+							await new Promise(r => setTimeout(r, 500));
 							buffer[0] = 0x0d;
 							port.write(buffer);
 						}
